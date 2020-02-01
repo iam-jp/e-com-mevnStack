958320a60b77502789b425250ab2de9601d3f70c
Jp <hclyogesh@gmail.com>
2020-02-01 22:40:58 +0530

    https server

--- src/index.js
+++ src/index.js
@@ -3,6 +3,7 @@ const express = require('express')
 const cors = require ('cors')
 const path = require ('path')
 const logger = require ('morgan')
+const https = require('https')
 const history = require('connect-history-api-fallback')
 const bannerUploadRouter = require('./routes/bannerUpload-r') 
 const storeRouter = require('./routes/stores-r')
@@ -20,6 +21,8 @@ const port = process.env.PORT || 5000
 const staticFileMiddleware = express.static(path.join(__dirname))
 app.use(cors())
 app.use(express.json())
+// app.use(https)
+const server = https.createServer(app)
 app.use(staticFileMiddleware)
 app.use(history())
 app.use(staticFileMiddleware)
@@ -48,8 +51,8 @@ app.use(function(req, res, next) {
 
 
 
-app.listen(port,()=>{
-    console.log('server started on port ' + port)
-})
+// app.listen(port,()=>{
+//     console.log('server started on port ' + port)
+// })
 
-
+server.listen(port)
